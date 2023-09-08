681df22b9c01c01f0eadd32effe5ac346ce757a3
zgj <2369394195@qq.com>
2023-09-08 15:28:55 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from "./components/index.vue";
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <router-view></router-view>
+</template>
---
+++ src/components/map.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="mapbox">
+    <div id="mars3dContainer"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+//引入cesium基础库
+import * as mars3d from "mars3d";
+import options from "./img/data-view-config.json";
+// import "mars3d-cesium/Build/Cesium/Widgets/widgets.css";
+function initmap() {
+  var map = new mars3d.Map("mars3dContainer", options);
+  console.log(map);
+}
+onMounted(() => {
+  initmap();
+});
+</script>
+<style lang="scss" scoped>
+#mapbox {
+  width: 100%;
+  height: 100%;
+  // overflow: hidden;
+  // position: relative;
+  // border: 1px solid red;
+}
+#mars3dContainer {
+  width: 100%;
+  height: 100%;
+  // border: 1px solid yellow;
+}
+</style>
